984aac3e038d7670701eaa261aa048a3c6f77b55
Adukassie <adugnadagnaw01@gmail.com>
2023-02-24 13:12:36 -0600

    complete the Oath functionality

--- src/component/OAuth.js
+++ src/component/OAuth.js
@@ -1,10 +1,43 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
 import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
+import { db } from "../firebase";
+import { Link, useNavigate } from "react-router-dom";
 import { FaFacebook } from "react-icons/fa";
 
 function OAuth() {
+  const navigate = useNavigate();
+  async function onGoogleClick() {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      // console.log(user);
+      //  1st check for the user
+      const docRef = doc(db, "users", user.uid); //address
+      const docSnap = await getDoc(docRef); //promise
+
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+         navigate("/");
+    } catch (error) {
+      toast.error("Could not authorize with Google");
+    }
+  }
+
   return (
     <>
       <button
+        type="button"
+        onClick={onGoogleClick}
         className="flex items-center
      justify-center w-full
     
@@ -45,7 +78,8 @@ function OAuth() {
           className="text-2xl 
        rounded-full mr-12"
         />
-        Continue with Facebook
+        <a href={"https://www.facebook.com/"}>Continue with Facebook</a>
+        {/* Continue with Facebook */}
       </button>
     </>
   );
